d254f140ae0476fe415ab321e99fa570410acd7f
hiys <43769085+hiys@users.noreply.github.com>
2019-04-04 05:35:24 -0500

    Add files via upload

--- pythonScripts/day05/tryExceptElseFinallyRaise.py
+++ pythonScripts/day05/tryExceptElseFinallyRaise.py
@@ -12,7 +12,7 @@ def  solution_error():
     print('\033[32;46;1m 100/num is %f\033[0m' % result, \
     end= ' this is result\n')
   except  ValueError:
-    print('数据类型错误,或者是空输入,请输入整数')
+    print('输入的数据类型错误,或者是空输入,请输入整数')
   except  ZeroDivisionError:
     print('数字0 不能使用,除数不能是零!')
   except  KeyboardInterrupt:
@@ -84,6 +84,35 @@ def  solution_error5():
   print('end of Exception capture program \n \
   当前面出现 "except 语句" 所 没有捕获到的异常后,本语句也不会执行---')
 
+def  set_age(name,age):
+  try:  
+    print('%s is %d years old' % (name, age))
+  except  TypeError as  te:
+    print('\n #变量类型错误,应该是int(age)\t',te)
+    print('%s is %d years old' % (name, int(age)))
+    print('--------------------------')
+    if  not  0< int(age) < 100:
+      raise  ValueError("输入的数据错误,超出规定范围")
+   # raise 语句能够主动触发异常
+    print('-----如果执行了raise 语句,此行代码不会输出----')
+    print('%s is %s  old' % (name,age))     
+  # 断言是一句必须等价于布尔值为真的判定
+    assert  name  in  ['peri','hiys','qq'], \
+      'name is out of listRange---AssertionError:'
+
+    print('-**-如果执行了assert 语句,此行代码不会输出-*-')
+    print('name is %s' % name)
+  else:   #不发生异常才执行的语句
+    print('\033[32;46;1m#不发生异常才执行的语句\033[0m', \
+    end= ' this is else\n')
+  finally:
+    print('不管是否异常都会执行的finally 语句')
+  print('end of Exception capture program \n \
+  当前面出现 "except 语句" 所 没有捕获到的异常后,本语句也不会执行---')
+
+
+
+
 if __name__ == '__main__':
 
   print('\033[30;43;1m sys.argv[0]  is %s \033[0m' % sys.argv[0])
@@ -91,6 +120,10 @@ if __name__ == '__main__':
 #  solution_error2()
 #  solution_error3()
 #  solution_error4()
-  solution_error5()
-
+#  solution_error5()
+  try:
+    set_age(sys.argv[1],sys.argv[2])
+  except IndexError as  ie:
+    print('\nIndexError 没有输入位置参数,序列中没有没有此索引\n',ie)
+  
 
