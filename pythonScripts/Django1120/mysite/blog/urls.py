2b217da876f17dd0fa6a5758d5fd5c32f3dcdd0b
hiys <43769085+hiys@users.noreply.github.com>
2019-05-01 02:23:35 -0500

    Add files via upload

--- pythonScripts/Django1120/mysite/blog/urls.py
+++ pythonScripts/Django1120/mysite/blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from  .views  import  index, hanshu, indexx
-
+from  .views  import  index, hanshu, indexx, test
 
 urlpatterns = [
     url(r'^$', index),
     url(r'^indexx$', indexx),
     url(r'^indexxx', hanshu),
+    url(r'^t', test),
 ]
--- pythonScripts/Django1120/mysite/blog/views.py
+++ pythonScripts/Django1120/mysite/blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from  .models  import  Post
 
 # Create your views here.
 
 def  index(request):
   page = request.GET.get('page');
-  return render(request, "index.html")
+  listPost = Post.objects.all()
+  return render(request, "index.html", {'lstPost': listPost})
 def  indexx(request):
   return render(request, "index.html")
 def  hanshu(request):
   return HttpResponse("中文页面.Django/mysite/blog/views.py---hanshu");
 
+def  test(request):
+  a = [0, 1, 2, 3, 4]
+  b = 'hello world'
+  return render(request,'t.html', {'a':a, 'b' : b})
