65c919246cf899b2eb4de2d266734f46e3656c73
hiys <43769085+hiys@users.noreply.github.com>
2019-05-23 23:07:17 -0500

    Add files via upload

---
+++ pythonScripts/PyScripts/PyScripts3/foobar.py
@@ -0,0 +1,10 @@
+# pwd  -> /var/ftp/py2018/py1801/day11
+# mkdir aaa
+# 新建aaa/foobar.py
+# 在day11目录下运行python3
+# >>> import aaa.foobar
+# >>> aaa.foobar.company
+# aaa目录被当成特殊的模块，叫作包
+
+
+company = 'tedu Ltd'
